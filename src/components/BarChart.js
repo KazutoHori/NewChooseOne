1a8a13eefe30995b58fd8ed62f686ac8fe420a24
Kazuto <kazutohori0714@icloud.com>
2021-06-23 02:48:12 +0900

    updated in 2021/06/22

---
+++ src/components/BarChart.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+export default class BarChart extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render () {
+
+    const { labels, values, colors } = this.props;
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          data: values,
+          backgroundColor: colors,
+          borderColor: colors,
+          hoverBackgroundColor: colors,
+          borderWidth: 2,
+          hoverBorderColor: colors,
+          hoverBorderWidth: 7,
+          borderSkipped: 'top'
+        }
+      ]
+    };
+
+    const options = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      animation: {
+        duration: 5000,
+      },
+      tooltips: {
+        backgroundColor: 'white',
+        titleFontColor: 'black',
+        caretSize: 5,
+        callbacks: {
+          labelColor: function(tooltipItem, chart){
+            return {
+              borderColor: 'white',
+              backgroundColor: 'white',
+            }
+          },
+          labelTextColor: function(tooltipItem, chart){
+            return 'black';
+          },
+        }
+      },
+      scales: {
+        xAxes: [{
+          barPercentage: .5,
+          categoryPercentage: .5,
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            callback: function(value) {if (value % 1 === 0) {return value;}}
+          }
+        }]
+      },
+      plugins: {
+        labels: {
+          render: 'value',
+          precision: 0,
+        }
+      },
+      layout: {
+        padding: {
+          top: 13,
+        },
+      },
+    };
+
+    return (
+      <Fragment>
+        <Bar data={data} options={options} />
+      </Fragment>
+    )
+  }
+}
--- src/screens/QuestionResult.js
+++ src/screens/QuestionResult.js
@@ -2,53 +2,179 @@ import React, { Component, Fragment } from 'react';
 import Loading from 'react-loading';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import QuestionList from '../components/QuestionList';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
 import ModalDelete from '../components/ModalDelete';
+import PieChart from '../components/PieChart';
+import BarChart from '../components/BarChart';
+import '../App.scss';
+
+// Firebase
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyArjDv3hS4_rw1YyNz-JFXDX1ufF72bqr8",
+  authDomain: "chooseone-105a9.firebaseapp.com",
+  databaseURL: "https://chooseone-default-rtdb.firebaseio.com",
+  projectId: "chooseone",
+  storageBucket: "chooseone.appspot.com",
+  messagingSenderId: "722704825746",
+  appId: "1:722704825746:web:73f11551b9e59f4bc2d54b",
+  measurementId: "G-YJ97DZH6V5"
+};
+if (firebase.apps.length === 0){ firebase.initializeApp(firebaseConfig); }
+var db = firebase.firestore();
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(3, 122, 255)',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    green: {
+      main: 'rgb(40, 168, 69)',
+    }
+  },
+});
 
 export default class QuestionResult extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      modalVisible: false,
+      user: null,
+      madeIt: null,
+      the_question: null,
+      your_vote: null,
+      likeIt: false,
+      choicesSorted: [],
+      labels: [],
+      values: [],
+      colors: [],
+    }
+  }
+
+  async componentDidMount() {
+    const { uid } = this.props;
+    const { the_slug } = this.props.match.params;
+    if(uid === null || this.state.the_question !== null) return null;
+
+    var user = {}
+    await db.collection('users').doc(uid).get().then((doc) => {
+      if(doc.exists){
+        user = doc.data()
+        this.setState({ user: doc.data() })
+      }
+    })
+    
+    if (user.question_liked.includes(the_slug)){
+      this.setState({ likeIt: true });
+    }else{
+      this.setState({ likeIt: false });
+    }
+
+    if (user.question_created.includes(the_slug)){
+      this.setState({ madeIt: true });
+    }else{
+      this.setState({ madeIt: false });
+    }
+
+    var the_question = null
+    await db.collection('questions').doc(the_slug).get().then((doc) => {
+      if(doc.exists){
+        the_question = doc.data()
+        this.setState({ the_question: doc.data() })
+      }else{
+        //TODO  ホームに戻る
+      }
+    });
+
+    if(the_question){
+      var copy=Array.from(the_question.choices);
+      copy.sort(function(first, second){
+        if (first.votes > second.votes){
+          return -1;
+        }else if (first.votes < second.votes){
+          return 1;
+        }else{
+          return 0;
+        }
+      });
+      this.setState({ choicesSorted: copy });
+      var l = [];
+      var v = [];
+      var c = [];
+
+      for(let i=0; i<copy.length;  i++){
+        l.push(copy[i].choice_text);
+        v.push(copy[i].votes);
+      }
+
+      // Chart.defaults.global.barPercentage=1.0;
+      var time = the_question.created_at
+      var seconds = time[-2]+time[-1]
+      if(v !== []){
+        v.forEach((entry, idx) => { // generate some colors
+          c.push('hsla('+((idx+seconds)*70)+',75%,75%,1)');
+        });
+      }
+      this.setState({ labels: l, values: v, colors: c });
+    }
+  }
+
+  onLikeit = () => {
+    const { likeIt, user, the_question } = this.state;
+    if(likeIt){
+      this.setState({ likeIt: false });
+      db.collection("users").doc(user.uid).update({
+        question_liked: firebase.firestore.FieldValue.arrayRemove(the_question.slug)
+      });
+    }else{
+      this.setState({ likeIt: true });
+      db.collection("users").doc(user.uid).update({
+        question_liked: firebase.firestore.FieldValue.arrayUnion(the_question.slug)
+      });
+    }
+  }
+
+  onDelete = () => {
+    const { the_choice, user, the_question } = this.state;
+
+    this.setState({ modalVisible: false });
+
+    db.collection("questions").doc(the_question.slug).delete();
+    db.collection("users").doc(user.uid).update({
+      question_created: firebase.firestore.FieldValue.arrayRemove(the_question.slug)
+    })
+
+    // navigate(from_where);
+  }
+
+  onClose = () => {
+    this.setState({ modalVisible: false });
+  }
+
   render() {
-    var madeIt = true;
-    var error_message = 'HELLO WORLD!!! THIS IS KAZUTO HORI';
-    var questions = [];
-    var your_vote = 'Soccer';
-    var the_question = {
-      all_votes: 917,
-      author: "JBMEXogvCuYl4m4dcVi4yJWnFpU2",
-      created_at: "2021/06/07 07:16:45",
-      created_on: "2021/06/07",
-      id: 13,
-      slug: "which-popular-sport-is-your-favorite",
-      title: "Which popular sport is your favorite?",
-      category: ['Pastime', 'Sports'],
-      choices: [
-        {
-          choice_text: "Basketball",
-          votes: 11,
-        },
-        {
-          choice_text: "Baseball",
-          votes: 8,
-        },
-        {
-          choice_text: "Hockey",
-          votes: 2,
-        },
-        {
-          choice_text: "Soccer",
-          votes: 1,
-        },
-      ],
-      comments: [],
-      users_answered: [],
-    };
-    var url = 'https://www.chooseone.app/'+the_question.slug;
-    var Ref = "https://twitter.com/share?url="+url+"/&text="+the_question.title;
-    var Fref = "https://www.facebook.com/share.php?u="+url;
-    var LRef = "https://social-plugins.line.me/lineit/share?url="+url;
+    const { madeIt, likeIt, the_question, your_vote, modalVisible, choicesSorted,
+        labels, values, colors } = this.state;
+
+    if(this.state.the_question === null){
+      return <Loading />
+    }
 
     return (
       <Fragment>
-        {/* ここからがメイン */}
         <div className="detail_container">
 
           {/* カテゴリー */}
@@ -67,10 +193,10 @@ export default class QuestionResult extends Component {
           </p>
 
           {/* モーダル */}
-          <ModalDelete />
+          {modalVisible &&  <ModalDelete onClose={this.onClose} onDelete={this.onDelete} />}
 
           {/* タイトル */}
-          <h3 className="center">{the_question.title}</h3>
+          <h3 className="center cali2">{the_question.title}</h3>
           <p className="date center">
             Created： {the_question.created_on}
           </p>
@@ -95,7 +221,7 @@ export default class QuestionResult extends Component {
               </tr>
             </thead>
             <tbody>
-              {the_question.choices.map((choice, idx) => (
+              {choicesSorted.map((choice, idx) => (
                 <tr id='table{idx+1}'>
                   <th scope="row">&nbsp;{idx+1}</th>
                   <td >{choice.choice_text}</td>
@@ -107,9 +233,10 @@ export default class QuestionResult extends Component {
 
           {/* グラフ */}
           <div className="graphs">
-            <div className="block pie-canvas"><canvas id="piechart" /></div>
-            <div className="block bar-canvas"><canvas id="barchart" /></div>
+            <div className="block pie-canvas"><PieChart labels={labels} values={values} colors={colors} /></div>
+            <div className="block bar-canvas"><BarChart labels={labels} values={values} colors={colors} /></div>
           </div>
+
           {/* ボタン系 */}
           <div className="buttons_normal">
             <form method="post">
@@ -137,14 +264,83 @@ export default class QuestionResult extends Component {
           {/* 似ている投稿 */}
           <div className="similar_posts main_list">
             <h3>Questions You May Like</h3>
-            <QuestionList questions={questions} />
-            {questions == [] && (
+            <QuestionList questions={[]} />
+            {/* {questions == [] && ( */}
               <pre>There are no similar posts yet.</pre>
-            )}
+            {/* )} */}
           </div>
         </div>
       </Fragment>
     )
+  }
+
+  async componentDidMount() {
+    const { uid } = this.props;
+    const { the_slug } = this.props.match.params;
+
+    if(uid === null || this.state.the_question !== null) return null;
+
+    var user = {}
+    await db.collection('users').doc(uid).get().then((doc) => {
+      if(doc.exists){
+        user = doc.data()
+        this.setState({ user: doc.data() })
+      }
+    })
+    
+    if (user.question_liked.includes(the_slug)){
+      this.setState({ likeIt: true });
+    }else{
+      this.setState({ likeIt: false });
+    }
+
+    if (user.question_created.includes(the_slug)){
+      this.setState({ madeIt: true });
+    }else{
+      this.setState({ madeIt: false });
+    }
+
+    var the_question = null
+    await db.collection('questions').doc(the_slug).get().then((doc) => {
+      if(doc.exists){
+        the_question = doc.data()
+        this.setState({ the_question: doc.data() })
+      }else{
+        //TODO  ホームに戻る
+      }
+    });
+
+    if(the_question){
+      var copy=Array.from(the_question.choices);
+      copy.sort(function(first, second){
+        if (first.votes > second.votes){
+          return -1;
+        }else if (first.votes < second.votes){
+          return 1;
+        }else{
+          return 0;
+        }
+      });
+      this.setState({ choicesSorted: copy });
+      var l = [];
+      var v = [];
+      var c = [];
+
+      for(let i=0; i<copy.length;  i++){
+        l.push(copy[i].choice_text);
+        v.push(copy[i].votes);
+      }
+
+      // Chart.defaults.global.barPercentage=1.0;
+      var time = the_question.created_at
+      var seconds = time[-2]+time[-1]
+      if(v !== []){
+        v.forEach((entry, idx) => { // generate some colors
+          c.push('hsla('+((idx+seconds)*70)+',75%,75%,1)');
+        });
+      }
+      this.setState({ labels: l, values: v, colors: c });
+    }
   }
 }
 
